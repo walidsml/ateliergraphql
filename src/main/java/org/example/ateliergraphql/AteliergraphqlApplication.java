825163204075df57470bbd88f8f54b9a14fb32cb
[walid] <[walid@gmail.com]>
2024-11-08 15:15:07 +0100

    Initial commit

---
+++ src/main/java/org/example/ateliergraphql/AteliergraphqlApplication.java
@@ -0,0 +1,13 @@
+package org.example.ateliergraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteliergraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteliergraphqlApplication.class, args);
+	}
+
+}
